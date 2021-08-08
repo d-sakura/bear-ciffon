9bebacdfb81b10e0e3d76546f50a7b611ec85d6f
d-sakura <d.sakura327@gmail.com>
2021-08-08 19:11:35 +0900

    各MENU、TOP修正

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,38 @@
-$(".openbtn").click(function(){//ボタンがクリックされたら
-    $(this).toggleClass('active');//ボタン自身にactiveクラスを付与
-    $("#g-nav-list").toggleClass('panelactive');//ナビにpanelactiveクラスを付与
-    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
-    $(".shop-link").toggleClass('active');
+$(function(){
 
-});
-
-$("#g-nav a").click(function(){//ナビのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンのactiveクラスを除去し
-    $("#g-nav-list").removeClass('panelactive');//ナビのpanelactiveクラスも除去
-    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
-    $(".shop-link").removeClass('active');
-});
-
-lightbox.option({
-    'albumLabel':""    
-});
-
-
-if ($('a[rel*=lightbox]').length > 0) {
-    $('a[rel*=lightbox]').lightbox({
-        overlayBgColor: '#fff',
-        overlayOpacoty: 0.6
+    $(".openbtn").click(function(){//ボタンがクリックされたら
+        $(this).toggleClass('active');//ボタン自身にactiveクラスを付与
+        $("#g-nav-list").toggleClass('panelactive');//ナビにpanelactiveクラスを付与
+        $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+        $(".shop-link").toggleClass('active');
+    
     });
-}
+    
+    $("#g-nav a").click(function(){//ナビのリンクがクリックされたら
+        $(".openbtn").removeClass('active');//ボタンのactiveクラスを除去し
+        $("#g-nav-list").removeClass('panelactive');//ナビのpanelactiveクラスも除去
+        $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+        $(".shop-link").removeClass('active');
+    });
+    
+
+    if ($('a[rel*=lightbox]').length > 0) {
+        lightbox.option({
+            'albumLabel':""    
+        });
+        $('a[rel*=lightbox]').lightbox({
+            overlayBgColor: '#fff',
+            overlayOpacoty: 0.6
+        });
+    }
+
+    $('a[href^="#"]').click(function(){
+        console.log(111)
+      let speed = 500;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
